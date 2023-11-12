d5d35505b16e5b7933717a28d7f863154c0e825b
Shirish <shirishaddaganti@gmail.com>
2023-11-12 01:01:02 -0500

    changed name

--- Part_1/streamlit_visual_search_artistic_style.py
+++ Part_1/streamlit_visual_search_artistic_style.py
@@ -98,7 +98,7 @@ if uploaded_image is not None:
         st.sidebar.image(uploaded_image, width=300)
         name = uploaded_image.name
         st.write(name)
-        search_by_style(image_style_embeddings, images, uploaded_image)
+        search_by_style(image_style_embeddings, images, name)
         #plt.savefig('search_by_style.png')
         st.header("Matching Images:")
         st.image('search_by_style.png')
