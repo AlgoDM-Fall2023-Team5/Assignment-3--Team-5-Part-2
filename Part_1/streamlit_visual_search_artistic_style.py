8c06bde5bf9712b7c46545f89c802a8b52361db9
saisrinivas-madamraju <145714507+saisrinivas-madamraju@users.noreply.github.com>
2023-11-13 13:49:45 +0000

    update

--- Part_1/streamlit_visual_search_artistic_style.py
+++ Part_1/streamlit_visual_search_artistic_style.py
@@ -1,23 +1,44 @@
-import streamlit as st
-import pickle
+import tensorflow as tf
+from tensorflow.keras.applications.vgg19 import preprocess_input
+from tensorflow.keras.models import Model
+print(tf.__version__)
 import matplotlib.pyplot as plt
-import matplotlib.offsetbox as offsetbox
+plt.rcParams.update({'pdf.fonttype': 'truetype'})
+from matplotlib import offsetbox
 import numpy as np
+from tqdm import tqdm
+import streamlit as st
+from PIL import Image
+import glob
+import ntpath
+import cv2
+import os
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import manifold
 import scipy as sc
-import cv2
-import ntpath
-import tensorflow as tf
-import glob
-from PIL import Image
 
-st.header("Visual Search Aristic Style")
-# D:\Projects\ADM Assg 3\Assignment-3--Team-5\Part_1\streamlit_visual_search_artistic_style.py
-#model = tf.keras.models.load_model("Part_1/visual_search_artistic.keras")
 
-with open('C:/Users/shiri/Documents/assg3Clone/Assignment-3--Team-5-Part-2/Part_1/image_style_embeddings.pickle', 'rb') as f:
-    image_style_embeddings = pickle.load(f)
+st.set_page_config(layout='wide')
+st.title("Artistic Style Search")
+
+image_paths = glob.glob('/workspaces/Assignment-3--Team-5-Part-2/Part_1/search/images-by-style/*.jpg')
+print(f'Found [{len(image_paths)}] images')
+
+images = {}
+for image_path in image_paths:
+    image = cv2.imread(image_path, 3)
+    b,g,r = cv2.split(image)           # get b, g, r
+    image = cv2.merge([r,g,b])         # switch it to r, g, b
+    image = cv2.resize(image, (200, 200))
+    images[ntpath.basename(image_path)] = image      
+
+n_col = 8
+n_row = int(len(images)/n_col)
+f, ax = plt.subplots(n_row, n_col, figsize=(16, 8))
+for i in range(n_row):
+    for j in range(n_col):
+        ax[i, j].imshow(list(images.values())[n_col*i + j])
+        ax[i, j].set_axis_off()
 
 def load_image(image):
     image = plt.imread(image)
@@ -26,7 +47,102 @@ def load_image(image):
     img = img[tf.newaxis, :] # shape -> (batch_size, h, w, d)
     return img
 
+#
+# content layers describe the image subject
+#
+content_layers = ['block5_conv2'] 
+
+#
+# style layers describe the image style
+# we exclude the upper level layes to focus on small-size style details
+#
+style_layers = [ 
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1', 
+        #'block4_conv1', 
+        #'block5_conv1'
+    ] 
+
+def selected_layers_model(layer_names, baseline_model):
+    outputs = [baseline_model.get_layer(name).output for name in layer_names]
+    model = Model([vgg.input], outputs)
+    return model
+
+# style embedding is computed as concatenation of gram matrices of the style layers
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+    return result/(num_locations)
+
+class StyleModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleModel, self).__init__()
+        self.vgg =  selected_layers_model(style_layers + content_layers, vgg)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    def call(self, inputs):
+        # scale back the pixel values
+        inputs = inputs*255.0
+        # preprocess them with respect to VGG19 stats
+        preprocessed_input = preprocess_input(inputs)
+        # pass through the reduced network
+        outputs = self.vgg(preprocessed_input)
+        # segregate the style and content representations
+        style_outputs, content_outputs = (outputs[:self.num_style_layers],
+                                          outputs[self.num_style_layers:])
+
+        # calculate the gram matrix for each layer
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+
+        # assign the content representation and gram matrix in
+        # a layer by layer fashion in dicts
+        content_dict = {content_name:value
+                    for content_name, value
+                    in zip(self.content_layers, content_outputs)}
+
+        style_dict = {style_name:value
+                  for style_name, value
+                  in zip(self.style_layers, style_outputs)}
+
+        return {'content':content_dict, 'style':style_dict}
+
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+
+def image_to_style(image_tensor):
+    extractor = StyleModel(style_layers, content_layers)
+    return extractor(image_tensor)['style']
+
+def style_to_vec(style):
+    # concatenate gram matrics in a flat vector
+    return np.hstack([np.ravel(s) for s in style.values()]) 
+
+#
+# Print shapes of the style layers and embeddings
+#
+
+image_tensor = load_image(image_paths[0])
+style_tensors = image_to_style(image_tensor)
+for k,v in style_tensors.items():
+    print(f'Style tensor {k}: {v.shape}')
+style_embedding = style_to_vec( style_tensors )
+print(f'Style embedding: {style_embedding.shape}')
+
+#
+# compute styles
+#
+image_style_embeddings = {}
+for image_path in tqdm(image_paths): 
+    image_tensor = load_image(image_path)
+    style = style_to_vec( image_to_style(image_tensor) )
+    image_style_embeddings[ntpath.basename(image_path)] = style
+#
 # Visualize the 2D-projection of the embedding space with example images (thumbnails)
+#
 def embedding_plot(X, images, thumbnail_sparsity = 0.005, thumbnail_size = 0.3):
     x_min, x_max = np.min(X, axis=0), np.max(X, axis=0)
     X = (X - x_min) / (x_max - x_min)
@@ -40,17 +156,8 @@ def embedding_plot(X, images, thumbnail_sparsity = 0.005, thumbnail_size = 0.3):
         ax.add_artist(offsetbox.AnnotationBbox(thumbnail, X[i], bboxprops = dict(edgecolor='white'), pad=0.0))
 
     plt.grid(True)
+    st.pyplot(fig)
 
-
-# embedding_plot(X_tsne, images=list(images.values()))
-
-# # Save the figure as an image
-# plt.savefig('embedding_plot.png')
-
-# # Pass the saved image to the st.image() function
-# st.image('embedding_plot.png')
-
-# Search in the Embedding Space
 def search_by_style(image_style_embeddings, images, reference_image, max_results=10):
     v0 = image_style_embeddings[reference_image]
     distances = {}
@@ -65,53 +172,47 @@ def search_by_style(image_style_embeddings, images, reference_image, max_results
         ax[i].imshow(images[img[0]])
         ax[i].set_axis_off()
 
-    plt.show()
+    most_similar_images = sorted_neighbors[:max_results]
+    similar_image_paths = [img[0] for img in most_similar_images if img[0] in images]
+
+    st.pyplot(f)
+    return similar_image_paths
     
-
-image_paths = glob.glob('search/images-by-style/*.jpg')
-# print(f'Found [{len(image_paths)}] images')
-images = {}
-for image_path in image_paths:
-    image = cv2.imread(image_path, 3)
-    b,g,r = cv2.split(image) # get b, g, r
-    image = cv2.merge([r,g,b]) # switch it to r, g, b
-    image = cv2.resize(image, (200, 200))
-    images[ntpath.basename(image_path)] = image 
-
-tsne = manifold.TSNE(n_components=2, init='pca', perplexity=10, random_state=0)
-X_tsne = tsne.fit_transform( np.array(list(image_style_embeddings.values())) )
-
-# images mostly match the reference style, although not perfectly
-#image = Image.open('Part_1/Arn-Van-Gogh-Secondary-1.jpg')
-
-# search_by_style(image_style_embeddings, images, 's_impressionist-02.jpg')
-# plt.savefig('style_plot.png')
-# st.image('style_plot.png')
-# images mostly match the reference style, although not perfectly
-uploaded_image = st.file_uploader("Upload an Image", type=['jpg', 'png', 'jpeg'])
-# st.write(uploaded_image)
-
-if uploaded_image is not None:
-    st.write(":green[Image Uploaded Successfully]")
-    st.sidebar.write(":blue[Uploaded Image:]")
-    st.sidebar.image(uploaded_image, width=300)
-    name = uploaded_image.name
-    st.write(name)
-    search_by_style(image_style_embeddings, images, name)
-    
-    #plt.savefig('search_by_style.png')
-    st.header("Matching Images:")
-    st.image('search_by_style.png')
-
-    embedding_plot(X_tsne, images=list(images.values()))
-
-    # Save the figure as an image
-    
-    #plt.savefig('embedding_plot.png')
-
-    # Pass the saved image to the st.image() function
-    st.header("2D Embedding Plot:")
-    st.image('embedding_plot.png')
-
-
-    st.error(f'Failed to load or process the image: {str(e)}')
+tab1, tab2 = st.tabs(["Image Search","Embedding Plot"])
+uploaded_file = st.file_uploader("Choose a image file", type=".jpg")
+image_style_embeddings_new = {}
+save_path = ""
+with tab1:
+    if uploaded_file is not None:
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        st.image(opencv_image, channels="BGR")
+        image_path = os.path.abspath(uploaded_file.name)
+        image_tensor_new = load_image(image_path)
+        style = style_to_vec(image_to_style(image_tensor_new) )
+        image_style_embeddings[ntpath.basename(image_path)] = style
+        st.write("Similiar Images by Style:")
+        similiar_images = search_by_style(image_style_embeddings, images, uploaded_file.name)
+        for image_p in similiar_images:
+            images_path = "tensor-house-data-master/search/images-by-style/"+image_p
+            image = cv2.imread(images_path)
+            if image is not None:
+                filename = ntpath.basename(image_p)
+                save_path = os.path.join("plot_images", filename)
+                cv2.imwrite(save_path, image)
+                print(f"Image saved to: {save_path}")
+with tab2:
+    images = {}
+    if uploaded_file is not None:
+        save = glob.glob("plot_images/*.jpg")
+        for image in save:
+            img = cv2.imread(image, 3)
+            b,g,r = cv2.split(img)           
+            img = cv2.merge([r,g,b])  
+            images[ntpath.basename(image)] = img
+            image_tensor = load_image(image)
+            style = style_to_vec(image_to_style(image_tensor))
+            image_style_embeddings_new[ntpath.basename(image)] = style   
+        tsne = manifold.TSNE(n_components=2, init='pca', perplexity=5, random_state=0)
+        X_tsne = tsne.fit_transform(np.array(list(image_style_embeddings_new.values())))
+        embedding_plot(X_tsne, images=list(images.values()))
